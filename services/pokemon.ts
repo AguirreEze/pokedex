652edec6535fd10b19f83e476a0741670db5c949
AguirreEze <aguirre.eze95@gmail.com>
2022-05-20 11:14:59 -0300

    feat getPokemon service

--- services/pokemon.ts
+++ services/pokemon.ts
@@ -6,3 +6,10 @@ export function getPokemonList<T>(): Promise<T> {
   }
   return fetch(baseURL, headers).then((res) => res.json())
 }
+
+export function getPokemon<T>(pokemon: string): Promise<T> {
+  const headers = {
+    method: "GET",
+  }
+  return fetch(`${baseURL}${pokemon}`, headers).then((res) => res.json())
+}
