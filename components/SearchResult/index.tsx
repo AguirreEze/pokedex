957d604fac8858cf390191eac2aa708391bded88
AguirreEze <aguirre.eze95@gmail.com>
2022-05-20 16:52:29 -0300

    wop

--- components/SearchResult/index.tsx
+++ components/SearchResult/index.tsx
@@ -1,10 +1,15 @@
-import { PokemonSearchType } from "types"
+import Link from "next/link"
+import { PokemonResultType } from "types"
 import styles from "./styles.module.css"
 
 interface Iprops {
-  data: PokemonSearchType
+  data: PokemonResultType
 }
 
 export default function SearchResult({ data }: Iprops) {
-  return <li className={styles.item}>{data.name}</li>
+  return (
+    <Link href={`/pokemon/${data.name}`}>
+      <li className={styles.item}>{data.name}</li>
+    </Link>
+  )
 }
--- services/pokemon.ts
+++ services/pokemon.ts
@@ -1,11 +1,13 @@
 import { SearchType } from "types"
 
+const PokemonPerPage = 20
+
 const baseURL = "https://pokeapi.co/api/v2/pokemon?"
 
 export function getPokemonList<T>(search: SearchType): Promise<T> {
   const params = {
-    offset: `${20 * search.page - 20}`,
-    limit: "20",
+    offset: `${PokemonPerPage * search.page - PokemonPerPage}`,
+    limit: `${PokemonPerPage}`,
   }
   const urlParams = new URLSearchParams(params).toString()
   const headers = {
