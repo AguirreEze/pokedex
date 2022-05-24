133586bc72eed69817e2ffa8721a4b1660f98be3
AguirreEze <aguirre.eze95@gmail.com>
2022-05-24 10:34:56 -0300

    fix merge bug

--- services/pokemon.ts
+++ services/pokemon.ts
@@ -2,7 +2,7 @@ import { SearchType } from "types"
 
 const PokemonPerPage = 20
 
-const baseURL = "https://pokeapi.co/api/v2/pokemon?"
+const baseURL = "https://pokeapi.co/api/v2/pokemon"
 
 export function getPokemonList<T>(search: SearchType): Promise<T> {
   const params = {
@@ -13,12 +13,12 @@ export function getPokemonList<T>(search: SearchType): Promise<T> {
   const headers = {
     method: "GET",
   }
-  return fetch(baseURL + urlParams, headers).then((res) => res.json())
+  return fetch(baseURL + "?" + urlParams, headers).then((res) => res.json())
 }
 
 export function getPokemon<T>(pokemon: string): Promise<T> {
   const headers = {
     method: "GET",
   }
-  return fetch(`${baseURL}${pokemon}`, headers).then((res) => res.json())
+  return fetch(`${baseURL}/${pokemon}`, headers).then((res) => res.json())
 }
