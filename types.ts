6d1e189016abb5d7b1a310c95a8c3a7a87c3868d
AguirreEze <aguirre.eze95@gmail.com>
2022-05-25 09:56:37 -0300

    fix Typo

--- types.ts
+++ types.ts
@@ -52,11 +52,11 @@ export interface PokemonData {
   }[]
   sprites: {
     back_default: string
-    back_femae: string
+    back_female: string
     back_shiny: string
     back_shiny_female: string
     front_default: string
-    front_femae: string
+    front_female: string
     front_shiny: string
     front_shiny_female: string
   }
