b2263e7ec82aa4bbd52877120183fccf8af02f3d
AguirreEze <aguirre.eze95@gmail.com>
2022-05-25 12:06:01 -0300

    Feat RawDataFilter

--- types.ts
+++ types.ts
@@ -16,50 +16,52 @@ export interface SearchType {
 
 export interface PokemonData {
   name: string
+  height: number
+  weight: number
+  baseExperience: number
+  sprites: {
+    backDefault: string
+    frontDefault: string
+    backShiny: string
+    frontShint: string
+  }
+  abilities: {
+    name: string
+    isHidden: boolean
+    slot: number
+  }[]
+  stats: {
+    name: string
+    effort: number
+    baseStats: number
+  }[]
+  types: {
+    name: string
+    slot: number
+    doubleDamageFrom: string[]
+    doubleDamageTo: string[]
+    halfDamageFrom: string[]
+    halfDamageTo: string[]
+    noDamageFrom: string[]
+    noDamageTo: string[]
+  }[]
+  heldItems: {
+    name: string
+    effect: string
+  }[]
+}
+
+export interface PokemonDataRaw {
+  name: string
   id: number
   height: number
   weight: number
-  abilities: {
-    ability: {
-      name: string
-      url: string
-    }
-    is_hidden: boolean
-    slot: number
-  }[]
-  held_items: {
-    item: {
-      name: string
-      url: string
-    }
-    version_details: []
-  }[]
+  abilities: PokemonAbilitiesRaw[]
+  held_items: PokemonHeldItemsRaw[]
   base_experience: number
-  stats: {
-    base_stat: number
-    effort: number
-    stat: {
-      name: string
-      url: string
-    }
-  }[]
-  types: {
-    slot: number
-    type: {
-      name: string
-      url: string
-    }
-  }[]
-  sprites: {
-    back_default: string
-    back_female: string
-    back_shiny: string
-    back_shiny_female: string
-    front_default: string
-    front_female: string
-    front_shiny: string
-    front_shiny_female: string
-  }
+  stats: PokemonStatsRaw[]
+  types: PokemonTypesRaw[]
+  sprites: PokemonSpritesRaw
   moves: {
     move: {
       name: string
@@ -71,4 +73,49 @@ export interface PokemonData {
     name: string
     url: string
   }[]
+}
+
+export interface PokemonAbilitiesRaw {
+  ability: {
+    name: string
+    url: string
+  }
+  is_hidden: boolean
+  slot: number
+}
+
+export interface PokemonTypesRaw {
+  slot: number
+  type: {
+    name: string
+    url: string
+  }
+}
+
+export interface PokemonStatsRaw {
+  base_stat: number
+  effort: number
+  stat: {
+    name: string
+    url: string
+  }
+}
+
+export interface PokemonHeldItemsRaw {
+  item: {
+    name: string
+    url: string
+  }
+  version_details: []
+}
+
+export interface PokemonSpritesRaw {
+  back_default: string
+  back_female: string
+  back_shiny: string
+  back_shiny_female: string
+  front_default: string
+  front_female: string
+  front_shiny: string
+  front_shiny_female: string
 }
